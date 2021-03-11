e5385de813e319707d11f20d3c94a0fe188dd175
KyungHyunKoo <poyo062@aradnetworks.com>
2021-03-11 16:55:00 +0900

    Layout Setup

--- app/src/main/java/com/example/myfitnessassistant/MainActivity.java
+++ app/src/main/java/com/example/myfitnessassistant/MainActivity.java
@@ -1,14 +1,62 @@
 package com.example.myfitnessassistant;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    Toolbar mToolbar;
+    ActionBar mActionbar;
+    RecyclerView routineList;
+    ArrayList<String> routineItems;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.action_bar, menu);
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /* Toolbar Setup */
+        mToolbar = findViewById(R.id.toolbar);
+        mToolbar.setTitleTextColor(Color.WHITE);
+        setSupportActionBar(mToolbar);
+        mActionbar = getSupportActionBar();
+        mActionbar.setDisplayShowCustomEnabled(true);
+        mActionbar.setDisplayShowTitleEnabled(true);
+        mActionbar.setHomeButtonEnabled(true);
+        mActionbar.setDisplayHomeAsUpEnabled(true);
+        mActionbar.setHomeAsUpIndicator(R.drawable.ic_baseline_account_circle_20);
+
+
+        /* Routine List Setup */
+        routineList = findViewById(R.id.list_routines);
+
+
+        ImageButton mWorkOutAddButton = findViewById(R.id.btn_addRoutine);
+        mWorkOutAddButton.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,MakeRoutineActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/myfitnessassistant/MakeRoutineActivity.java
@@ -0,0 +1,14 @@
+package com.example.myfitnessassistant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MakeRoutineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make_routine);
+    }
+}
