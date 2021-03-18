ad7c9366bc620d5d3635e19fee29747eb5cbc679
KyungHyunKoo <poyo062@aradnetworks.com>
2021-03-18 16:41:47 +0900

    MaterialCalendarView Library Changed

--- app/src/main/java/com/example/myfitnessassistant/CalendarActivity.java
+++ app/src/main/java/com/example/myfitnessassistant/CalendarActivity.java
@@ -1,107 +1,79 @@
 package com.example.myfitnessassistant;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.constraintlayout.motion.widget.MotionHelper;
 import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.provider.CalendarContract;
 import android.text.style.ForegroundColorSpan;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CalendarView;
-import android.widget.DatePicker;
 import android.widget.Toast;
 
-import com.prolificinteractive.materialcalendarview.CalendarDay;
-import com.prolificinteractive.materialcalendarview.CalendarMode;
-import com.prolificinteractive.materialcalendarview.DayViewDecorator;
-import com.prolificinteractive.materialcalendarview.DayViewFacade;
-import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
-import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
-import com.prolificinteractive.materialcalendarview.OnRangeSelectedListener;
+import com.applandeo.materialcalendarview.CalendarView;
+import com.applandeo.materialcalendarview.DatePicker;
+import com.applandeo.materialcalendarview.EventDay;
+import com.applandeo.materialcalendarview.listeners.OnDayClickListener;
 
-import org.threeten.bp.DayOfWeek;
-import org.threeten.bp.LocalDate;
-
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
-public class CalendarActivity extends AppCompatActivity implements OnDateSelectedListener, View.OnClickListener {
+public class CalendarActivity extends AppCompatActivity implements OnDayClickListener, View.OnClickListener {
 
-    private MaterialCalendarView mMaterialCalendarView;
-    private CalendarDay mDate;
+    CalendarView mCalendarView;
+    private Toolbar mToolbar;
+    private ActionBar mActionbar;
+    private EventDay mDate;
     private Button mTestButton;
+    List<EventDay> mEventDays = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
 
-        mMaterialCalendarView = findViewById(R.id.calendarView);
+        mToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        mActionbar = getSupportActionBar();
+        mActionbar.setDisplayShowCustomEnabled(true);
+        mActionbar.setDisplayShowTitleEnabled(true);
+        mActionbar.setHomeButtonEnabled(true);
+        mActionbar.setDisplayHomeAsUpEnabled(true);
+        mActionbar.setHomeAsUpIndicator(R.drawable.ic_baseline_account_circle_20);
 
-        mMaterialCalendarView.addDecorators(new SundayDecorator(),
-                                            new SaturdayDecorator());
-        mMaterialCalendarView.state().edit()
-                .setFirstDayOfWeek(DayOfWeek.SUNDAY)
-                .setMinimumDate(CalendarDay.from(2016, 4, 3))
-                .setMaximumDate(CalendarDay.from(2030, 5, 12))
-                .setCalendarDisplayMode(CalendarMode.MONTHS)
-                .commit();
-        mMaterialCalendarView.setOnDateChangedListener(this::onDateSelected);
+        mCalendarView = findViewById(R.id.calendarView);
+        mCalendarView.setOnDayClickListener(this::onDayClick);
         findViewById(R.id.testButton).setOnClickListener(this::onClick);
     }
 
     @Override
-    public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
-//        Toast.makeText(this,"Date : " + date.getDate().toString(),Toast.LENGTH_SHORT).show();
-        mDate = date;
+    public void onDayClick(EventDay eventDay) {
+        mDate = eventDay;
+//        Toast.makeText(this,getFormattedDate(eventDay.getCalendar().getTime()),Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onClick(View view) {
-        if(mDate.getDay() == 30) {
-            Toast.makeText(this,"Test Success", Toast.LENGTH_SHORT).show();
-        }
+        Calendar mCalendar = mDate.getCalendar();
+        mEventDays.add(new EventDay(mCalendar,R.drawable.ic_baseline_eco_5));
+        mCalendarView.setEvents(mEventDays);
+        Toast.makeText(this,getFormattedDate(mCalendarView.getFirstSelectedDate().getTime()),Toast.LENGTH_SHORT).show();
     }
 
-    //TODO - 공휴일 api로 받아와서 일요일 + 공휴일 전부 빨간 날 처리하
-    private class SundayDecorator implements DayViewDecorator {
-        @Override
-        public boolean shouldDecorate(CalendarDay day) {
-            return day.getDate().getDayOfWeek().getValue() == 7;
-        }
-
-        @Override
-        public void decorate(DayViewFacade view) {
-            view.addSpan(new ForegroundColorSpan(Color.RED));
-        }
-    }
-
-    private class SaturdayDecorator implements DayViewDecorator {
-        @Override
-        public boolean shouldDecorate(CalendarDay day) {
-            return day.getDate().getDayOfWeek().getValue() == 6;
-        }
-
-        @Override
-        public void decorate(DayViewFacade view) {
-            view.addSpan(new ForegroundColorSpan(Color.BLUE));
-        }
-    }
-
-    private class EventDecorator implements DayViewDecorator {
-        @Override
-        public boolean shouldDecorate(CalendarDay day) {
-            return day.getDay() == 30;
-        }
-
-        @Override
-        public void decorate(DayViewFacade view) {
-            view.addSpan(new ForegroundColorSpan(Color.BLUE));
-        }
+    private static String getFormattedDate(Date date) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd MMMM yyyy", Locale.getDefault());
+        return simpleDateFormat.format(date);
     }
 
 }
